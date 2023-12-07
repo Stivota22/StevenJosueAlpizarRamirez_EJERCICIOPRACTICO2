396489b2b13eb21b43f73153dd87ab55dd48ca12
Steven A <60278482+Stivota22@users.noreply.github.com>
2023-12-06 19:14:51 -0600

    Commit parte 1

---
+++ com.EJERCICIOPRACTICO2/src/main/java/com/Ejercicio2/service/VuelosService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.Ejercicio2.service;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface VuelosService {
+    
+}
